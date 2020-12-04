101d54f2348e21f9534fc7c5ce65e1347bb61a22
Ryan King <rking@chanzuckerberg.com>
2020-12-04 11:02:28 -0800

    Revert "try dump"
    This reverts commit 111719604db340df08e4ff912ed34c1f41e09125.

--- main.go
+++ main.go
@@ -4,20 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/chanzuckerberg/go-misc/ver"
 	"github.com/chanzuckerberg/terraform-provider-snowflake/pkg/provider"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
 	version := flag.Bool("version", false, "spit out version for resources here")
 	flag.Parse()
-
-	spew.Dump(provider.Provider().Schema)
-	os.Exit(0)
 
 	if *version {
 		verString, err := ver.VersionStr()
